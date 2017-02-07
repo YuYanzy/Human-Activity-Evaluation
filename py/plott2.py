2ec9a1eb16ae0fe1bbd68b0b7a21a588b0ff3154
eirikaa <eirikaaboe@gmail.com>
2017-02-07 15:58:57 +0100

    some changes

--- py/plott2.py
+++ py/plott2.py
@@ -2,6 +2,9 @@
 
 import matplotlib.pyplot as plt
 import csv
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
 
 __author__ = 'eirikaa'
 
@@ -92,8 +95,7 @@ class AnalyseAccelerometer:
         """
 
         activity_threshold = 3
-        hard_activity_threshold = 20
-
+        hard_activity_threshold = 17
         activity = 40
         hard_activity = 45
         low_activity = -40
@@ -114,39 +116,51 @@ class AnalyseAccelerometer:
                 diff_class.append(hard_activity)
 
             elif diff >= activity_threshold:
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
-                diff_class.append(40)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
+                diff_class.append(activity)
 
             else:
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
-                diff_class.append(-40)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
+                diff_class.append(low_activity)
 
         return diff_class
 
     def geo_viz(self):
         # TODO: Folium, geopandas
-        pass
+
+        # points = pd.read_csv("data/testdata/02_03_geo_utenID.csv", delimiter=";")
+        points = pd.read_csv("data/log/02_06_geo.csv", delimiter=";")
+        points['geometry'] = points.apply(lambda z: Point(z.LAT, z.ID), axis=1)
+        print (points)
+        b = gpd.GeoDataFrame(points)
+
+        b.plot()
+        plt.show()
+
+
+        # TODO: fikse opp i data, mellomrom bør byttes med understrek
+        # TODO: Kutt ut ID intill det er fikset
 
 if __name__ == '__main__':
     # ana = AnalyseAccelerometer(filename='data/log/02_05_accelero.csv')
     # ana = AnalyseAccelerometer(filename='data/exampledata/tredemolle_accelero.csv')
-    ana = AnalyseAccelerometer(filename='data/calibration/calibration_z_accelero.csv')
+    ana = AnalyseAccelerometer(filename='data/exampledata/spinning_accelero.csv')
 
     x, y, z, xyz, time, activity = ana.readcsv()
     diff_xyz = ana.diff(x, y, z, xyz)
@@ -157,7 +171,8 @@ if __name__ == '__main__':
     diff_class.append(1)
     diff_class.append(1)
     diff_class.append(1)
-    diff_class.append(1)
+    # diff_class.append(1)
+    # diff_class.append(1)
     # diff_class.append(1)
     # diff_class.append(1)
     # diff_class.append(1)
@@ -165,5 +180,7 @@ if __name__ == '__main__':
     print(len(diff_class))
     print(len(xyz))
     ana.plot(x, y, z, xyz, time, diff_class)
+    #
+    # print (ana.calibration(xyz))
 
-    print (ana.calibration(xyz))
+    # ana.geo_viz()
--- py/store_data.py
+++ py/store_data.py
@@ -48,7 +48,7 @@ def index():
 def store_GNSS(lat=LAT,lon=LON,speed=SPEED,accuracy=ACCURACY,altitude=ALTITUDE,altitudeAcurracy=ALTITUDEACCURACY,heading=HEADING, activity = ACTIVITY):
     def write_csv(lat, lon, speed, accuracy, altitude, heading, time, activity, output_GNSS=GNSS_OUTPUT, fileformat=FILEFORMAT):
         # csv_writer = csv.writer(open(output_csv, "w"))
-        # # TODO: fikse time, få med navn
+
         # # csv_writer.writerow(["Index", "Lat", "Lon", "Speed", "Accuracy", "AltitudeAccuracy", "Heading", "X", "Y", "Z", "Time"])
         # csv_writer.writerow(["bla", lat, lon, speed, accuracy, altitudeAcurracy, heading, x, y, z, '\r\n'])
         # TODO: this must be moved, not sure how to solve this
@@ -60,8 +60,8 @@ def store_GNSS(lat=LAT,lon=LON,speed=SPEED,accuracy=ACCURACY,altitude=ALTITUDE,a
         #         i += 1
 
         with open('data/'+output_GNSS + str(i) + fileformat, "a+") as f:
-            f.write("bla" + " " + str(lat) + " " + str(lon) + " " + str(speed) + " " + str(accuracy) + " " +
-                    str(altitude) + " " + str(heading) + " " + str(time) + " " + str(activity) + " " + "\n")
+            f.write("bla" + ", " + str(lat) + ", " + str(lon) + ", " + str(speed) + ", " + str(accuracy) + ", " +
+                    str(altitude) + ", " + str(heading) + ", " + str(time) + ", " + str(activity) + " " + "\n")
 
     # GEOLOCATION
     lat = float((request.args.get("lat", lat)))
@@ -75,17 +75,17 @@ def store_GNSS(lat=LAT,lon=LON,speed=SPEED,accuracy=ACCURACY,altitude=ALTITUDE,a
     # TRUTH DATA
     activity = (request.args.get("activity", activity))
 
+    # TODO: fikse time? og heller konvertere ved plott?
+
     write_csv(lat, lon, speed, accuracy, altitude, heading, time.time(), activity)
-    #TODO: add normal time, easier to debug
-    return jsonify("bla" + " " + str(lat) + " " + str(lon) + " " + str(speed) + " " + str(accuracy) + " " + str(altitude) + " "
-                    + str(heading) + " " + str(time.time()) + " " + str(activity))
+    return jsonify("bla" + ", " + str(lat) + ", " + str(lon) + ", " + str(speed) + ", " + str(accuracy) + ", " + str(altitude) + ", "
+                    + str(heading) + ", " + str(time.time()) + ", " + str(activity))
 
 
 @app.route("/storeACCELEROMETER")
 def store_accelerometer(x=X,y=Y,z=Z, activity = ACTIVITY):
     def write_csv(x, y, z, time, activity, output_accelerometer=ACCELEROMETER_OUTPUT, fileformat=FILEFORMAT):
         # csv_writer = csv.writer(open(output_csv, "w"))
-        # # TODO: fikse time, få med navn
         # # csv_writer.writerow(["Index", "Lat", "Lon", "Speed", "Accuracy", "AltitudeAccuracy", "Heading", "X", "Y", "Z", "Time"])
         # csv_writer.writerow(["bla", lat, lon, speed, accuracy, altitudeAcurracy, heading, x, y, z, '\r\n'])
         # TODO: this must be moved, not sure how to solve this
@@ -98,8 +98,7 @@ def store_accelerometer(x=X,y=Y,z=Z, activity = ACTIVITY):
 
         with open('data/'+output_accelerometer + str(i) + fileformat, "a+") as f:
             f.write(
-                "bla" + " " + str(x) + " " + str(y) + " " + str(z) + " "+ str((math.sqrt((x)**2+(y)**2+(z)**2))-9.81) + " " + str(time) + " " + str(activity) + " " + "\n")
-    # TODO: add normal time, easier to debug
+                "bla" + ", " + str(x) + ", " + str(y) + ", " + str(z) + ", "+ str((math.sqrt((x)**2+(y)**2+(z)**2))-9.81) + ", " + str(time) + ", " + str(activity) + " " + "\n")
 
     # ACCELEROMETER
     x = float((request.args.get("x", x)))
@@ -109,9 +108,10 @@ def store_accelerometer(x=X,y=Y,z=Z, activity = ACTIVITY):
     # TRUTH DATA
     activity = (request.args.get("activity", activity))
 
+    # TODO: fikse time? og heller konvertere ved plott?
     write_csv(x, y, z, time.time(), activity)
 
-    return jsonify("bla" + " " + str(x) + " " + str(y) + " " + str(z) + " " + str(time.time()) + " " + str(activity))
+    return jsonify("bla" + ", " + str(x) + ", " + str(y) + ", " + str(z) + ", " + str((math.sqrt((x)**2+(y)**2+(z)**2))-9.81) +", " + str(time.time()) + ", " + str(activity))
 
 
 
